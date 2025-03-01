1e83af8969feea4148ae3d77bd81825606266986
jjvvll <llanetajohnver@gmail.com>
2025-03-02 00:14:59 +0800

    Daily Updates

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -12,7 +12,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class JobFactory extends Factory
 {
     /**
-     * Define the model's default state.///
+     * Define the model's default state.///////
      *
      * @return array<string, mixed>
      */
