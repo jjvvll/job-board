39ab87642081a5f03ea29c282f5a2ce48070b41c
jjvvll <llanetajohnver@gmail.com>
2025-03-16 10:55:14 +0800

    Daily Updates

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
+    <x-bread-crumbs class="mb-4"
+    :links="['Jobs' => route('jobs.index')]"/>
+
     @foreach ($jobs as $job)
         <x-job-card class="mb-4" :$job>
 
--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+    <x-bread-crumbs class="mb-4"
+    :links="['Jobs' => route('jobs.index'),  $job->title => '#']"/>
     <x-job-card  :$job/>
-
 </x-layout>
