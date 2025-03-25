2699b63a79ea31f9f0aad7699a26795c1da5ccb9
jjvvll <llanetajohnver@gmail.com>
2025-03-25 12:45:20 +0800

    Daily Updates

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,14 +1,14 @@
 <div class="relative">
-    @if ($formId)
+    @if ($formRef)
         <button type="button" class="absolute top-0 right-0 flex h-full items-center pr-2"
-        onclick="document.getElementById('{{$name}}').value=''; document.getElementById('{{$formId}}').submit()">
+        @click="$refs['input-{{$name}}'].value=''; $refs['{{$formRef}}'].submit();">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 text-slate-500">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
         </button>
     @endif
 
-    <input type="text" placeholder="{{$placeholder}}"
+    <input x-ref="input-{{$name}}" type="text" placeholder="{{$placeholder}}"
     name="{{$name}}" value="{{$value}}" id="{{$name}}"
     class="w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400
     focus:ring-2 pr-8"/>
